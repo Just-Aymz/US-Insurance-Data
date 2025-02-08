f46f4f2846fc35b3b22cc0d1c17a594c333a452e
Amogelang Amo More <bakwenaholdings01@gmail.com>
2025-02-08 05:33:54 +0200

    Updated Data_Preprocessing and Schema files

--- Schema.py
+++ Schema.py
@@ -25,5 +25,10 @@ class InputFeatures(BaseModel):
     smoker: SmokerStatus
     region: Region
 
+    """
+    The Config class with use_enum_values = True ensures that Pydantic 
+    uses the actual values of the Enum fields when serializing the data,
+    making the API response and request handling more user-friendly.
+    """
     class Config:
         use_enum_values = True
